a6aedd3b53cbf52b0ad5423fd194e5f84b87fd92
matiasp95 <matiaspaglia@gmail.com>
2023-06-11 19:41:00 -0300

    Modificaciones a content types

---
+++ src/api/link-destacado/controllers/link-destacado.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * link-destacado controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::link-destacado.link-destacado');
---
+++ src/api/link-destacado/services/link-destacado.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * link-destacado service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::link-destacado.link-destacado');
